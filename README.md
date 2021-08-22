728a21fb3f14e30adfcaca8190151c3a5a28413a
hritam_chowdhury <hritam.chowdhury@paytm.com>
2021-08-23 00:13:39 +0530

    commited all the ui changes

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+Complete front-end is built using reactJs framework and Back-end is built using Nodejs,ExpressJS and NoSql MongoDb.
+ The App is called "Album" and it allows users to post and keep those interesting events saved that happened in their lives.
+
+
---
+++ src/api/index.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+const PORT = process.env.PORT|| 5000;
+const url = `http://localhost:${PORT}/posts`;
+
+export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost);
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
